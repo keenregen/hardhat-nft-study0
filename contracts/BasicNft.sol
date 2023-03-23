d497a6882ba827abf5b1cdab996f1d9b91782119
Keen Regen <keenregen@gmail.com>
2023-03-23 19:18:02 +0300

    v1.0

--- contracts/BasicNft.sol
+++ contracts/BasicNft.sol
@@ -1,8 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.7;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
 // Basic nft Contract
 
-contract BasicNft {
+contract BasicNft is ERC721 {
+     uint256 private s_tokenCounter;
 
+     constructor() ERC721("nftZero", "ZERO") {
+          s_tokenCounter = 0;
+     }
+
+     function mintNft() public returns (uint256) {
+          _safeMint(msg.sender, s_tokenCounter);
+          s_tokenCounter = s_tokenCounter + 1;
+
+          return s_tokenCounter;
+     }
+
+     function getTokenCounter() public view returns (uint256) {
+          return s_tokenCounter;
+     }
 }
